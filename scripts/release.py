461de9fc8d89536cbaf403f846192084eecf2c20
teknium1 <127238744+teknium1@users.noreply.github.com>
2026-06-04 05:21:36 -0700

    chore(release): add AUTHOR_MAP entry for solaitken

--- scripts/release.py
+++ scripts/release.py
@@ -52,7 +52,6 @@ AUTHOR_MAP = {
     "ali.zakaee.1997@gmail.com": "ITheEqualizer",
     "copii.list@gmail.com": "stremtec",
     "solaiagent@gmail.com": "solaitken",
-    "cryptoworlldz@gmail.com": "worlldz",
     "prostoandrei9@gmail.com": "vladkvlchk",
     "116314616+ThyFriendlyFox@users.noreply.github.com": "ThyFriendlyFox",
     "liliangjya@gmail.com": "truenorth-lj",
