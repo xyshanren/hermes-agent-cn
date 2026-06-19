2c8a50a0e78b4f74d237b15b02431cd9944dafaa
teknium1 <127238744+teknium1@users.noreply.github.com>
2026-06-19 12:27:43 -0700

    chore: release v0.17.0 (2026.6.19)

--- hermes_cli/__init__.py
+++ hermes_cli/__init__.py
@@ -14,8 +14,8 @@ Provides subcommands for:
 import os
 import sys
 
-__version__ = "0.14.0+cn.3"
-__release_date__ = "2026.5.16"
+__version__ = "0.17.0"
+__release_date__ = "2026.6.19"
 
 
 def _ensure_utf8():
